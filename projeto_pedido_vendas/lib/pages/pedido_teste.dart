382b678a981cf4ddadf5ad4ac188f2ecec40ae1c
FernandoNicolini <fernando.juliano.nicolini@gmail.com>
2024-05-06 20:35:30 -0300

    att front 06/05

--- projeto_pedido_vendas/lib/pages/pedido_teste.dart
+++ projeto_pedido_vendas/lib/pages/pedido_teste.dart
@@ -13,6 +13,7 @@ import 'package:projeto_pedido_vendas/repository/itens_pedido.dart';
 import 'package:projeto_pedido_vendas/repository/pedido_dao.dart';
 import 'package:projeto_pedido_vendas/repository/produto_dao.dart';
 import 'package:projeto_pedido_vendas/repository/vendedor_dao.dart';
+import 'package:intl/intl.dart';
 
 class PedidoEmitirPageTeste extends StatefulWidget {
   const PedidoEmitirPageTeste({Key? key}) : super(key: key);
@@ -40,6 +41,10 @@ class _PedidoEmitirPageTesteState extends State<PedidoEmitirPageTeste> {
 
   final List<ProdutoDTO> _produtosSelecionados = [];
   final List<int> _quantidades = [];
+
+  final GlobalKey<AnimatedListState> _listKey = GlobalKey<AnimatedListState>();
+
+  String _observacoes = '';
 
   @override
   void initState() {
@@ -84,6 +89,8 @@ class _PedidoEmitirPageTesteState extends State<PedidoEmitirPageTeste> {
         _produtosSelecionados.add(_produtoSelecionado!);
         _quantidades.add(1); // Quantidade padrão ao adicionar um produto
       });
+
+      _listKey.currentState!.insertItem(_produtosSelecionados.length - 1);
     }
   }
 
@@ -101,12 +108,26 @@ class _PedidoEmitirPageTesteState extends State<PedidoEmitirPageTeste> {
     });
   }
 
+  void _removerProdutoDoCarrinho(int index) {
+    setState(() {
+      _produtosSelecionados.removeAt(index);
+      _quantidades.removeAt(index);
+    });
+
+    _listKey.currentState!.removeItem(
+        index,
+        (context, animation) => FadeTransition(
+              opacity: animation,
+              child: const SizedBox(),
+            ));
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: MinhaAppBar(),
-      drawer: MenuLateralEsquerdo(), // Adiciona o menu lateral direito
-      endDrawer: MenuLateralDireito(), // Adiciona o menu lateral esquerdo
+      drawer: MenuLateralEsquerdo(),
+      endDrawer: MenuLateralDireito(),
       body: Stack(
         children: [
           Container(
@@ -134,7 +155,13 @@ class _PedidoEmitirPageTesteState extends State<PedidoEmitirPageTeste> {
                   items: _clientesLista.map((ClienteDTO cliente) {
                     return DropdownMenuItem<ClienteDTO>(
                       value: cliente,
-                      child: Text(cliente.nome ?? ''),
+                      child: Row(
+                        children: [
+                          const Icon(Icons.person),
+                          const SizedBox(width: 8.0),
+                          Text(cliente.nome ?? ''),
+                        ],
+                      ),
                     );
                   }).toList(),
                   decoration: const InputDecoration(
@@ -142,26 +169,7 @@ class _PedidoEmitirPageTesteState extends State<PedidoEmitirPageTeste> {
                     border: OutlineInputBorder(),
                   ),
                 ),
-                const SizedBox(height: 16.0), // Espaçamento entre os dropdowns
-                DropdownButtonFormField<VendedorDTO>(
-                  value: _vendedorSelecionado,
-                  onChanged: (VendedorDTO? vendedor) {
-                    setState(() {
-                      _vendedorSelecionado = vendedor;
-                    });
-                  },
-                  items: _vendedoresLista.map((VendedorDTO vendedor) {
-                    return DropdownMenuItem<VendedorDTO>(
-                      value: vendedor,
-                      child: Text(vendedor.nome ?? ''),
-                    );
-                  }).toList(),
-                  decoration: const InputDecoration(
-                    labelText: 'Vendedor',
-                    border: OutlineInputBorder(),
-                  ),
-                ),
-                const SizedBox(height: 16.0), // Espaçamento entre os dropdowns
+                const SizedBox(height: 16.0),
                 DropdownButtonFormField<ProdutoDTO>(
                   value: _produtoSelecionado,
                   onChanged: (ProdutoDTO? produto) {
@@ -172,7 +180,13 @@ class _PedidoEmitirPageTesteState extends State<PedidoEmitirPageTeste> {
                   items: _produtosLista.map((ProdutoDTO produto) {
                     return DropdownMenuItem<ProdutoDTO>(
                       value: produto,
-                      child: Text(produto.nome ?? ''),
+                      child: Row(
+                        children: [
+                          const Icon(Icons.shopping_bag),
+                          const SizedBox(width: 8.0),
+                          Text(produto.nome ?? ''),
+                        ],
+                      ),
                     );
                   }).toList(),
                   decoration: const InputDecoration(
@@ -180,54 +194,89 @@ class _PedidoEmitirPageTesteState extends State<PedidoEmitirPageTeste> {
                     border: OutlineInputBorder(),
                   ),
                 ),
-                const SizedBox(height: 16.0), // Espaçamento entre os dropdowns
-                OutlinedButton(
-                  onPressed: _adicionarProdutoAoCarrinho,
-                  child: const Text('Adicionar ao Carrinho'),
+                const SizedBox(height: 16.0),
+                TextFormField(
+                  decoration: const InputDecoration(
+                    labelText: 'Observações',
+                    border: OutlineInputBorder(),
+                  ),
+                  maxLines: 3,
+                  onChanged: (value) {
+                    setState(() {
+                      _observacoes = value;
+                    });
+                  },
                 ),
-                const SizedBox(
-                    height:
-                        16.0), // Espaçamento entre o botão e a lista de produtos
-                ListView.builder(
+                const SizedBox(height: 16.0),
+                ElevatedButton(
+                  onPressed: _adicionarProdutoAoCarrinho,
+                  child: const Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Icon(Icons.add_shopping_cart),
+                      SizedBox(width: 8.0),
+                      Text('Adicionar ao Carrinho'),
+                    ],
+                  ),
+                ),
+                const SizedBox(height: 16.0),
+                AnimatedList(
+                  key: _listKey,
                   shrinkWrap: true,
-                  itemCount: _produtosSelecionados.length,
-                  itemBuilder: (context, index) {
-                    return Card(
-                      elevation: 4.0,
-                      margin: const EdgeInsets.symmetric(vertical: 8.0),
-                      child: ListTile(
-                        title: Text(_produtosSelecionados[index].nome ?? ''),
-                        subtitle: Row(
-                          children: [
-                            const Text('Quantidade: '),
-                            SizedBox(
-                              width: 50,
-                              child: TextFormField(
-                                initialValue: _quantidades[index].toString(),
-                                onChanged: (value) {
-                                  _alterarQuantidade(
-                                      index, int.tryParse(value) ?? 1);
-                                },
-                                keyboardType: TextInputType.number,
+                  initialItemCount: _produtosSelecionados.length,
+                  itemBuilder: (context, index, animation) {
+                    return SlideTransition(
+                      position: Tween<Offset>(
+                        begin: const Offset(1, 0),
+                        end: Offset.zero,
+                      ).animate(animation),
+                      child: Card(
+                        elevation: 4.0,
+                        margin: const EdgeInsets.symmetric(vertical: 8.0),
+                        child: ListTile(
+                          title: Text(_produtosSelecionados[index].nome ?? ''),
+                          subtitle: Row(
+                            children: [
+                              const Text('Quantidade: '),
+                              SizedBox(
+                                width: 50,
+                                child: TextFormField(
+                                  initialValue: _quantidades[index].toString(),
+                                  onChanged: (value) {
+                                    _alterarQuantidade(
+                                        index, int.tryParse(value) ?? 1);
+                                  },
+                                  keyboardType: TextInputType.number,
+                                ),
                               ),
-                            ),
-                            Text(
-                                'Total: ${_produtosSelecionados[index].valor * _quantidades[index]}'),
-                          ],
+                              Text(
+                                  'Total: ${_produtosSelecionados[index].valor * _quantidades[index]}'),
+                            ],
+                          ),
+                          trailing: IconButton(
+                            icon: const Icon(Icons.remove_circle),
+                            onPressed: () {
+                              _removerProdutoDoCarrinho(index);
+                            },
+                          ),
                         ),
                       ),
                     );
                   },
                 ),
-                const SizedBox(
-                    height:
-                        16.0), // Espaçamento entre a lista de produtos e o texto do total
+                const SizedBox(height: 16.0),
                 Card(
                   elevation: 4.0,
                   margin: const EdgeInsets.all(16.0),
                   child: Padding(
                     padding: const EdgeInsets.all(16.0),
-                    child: Text('Total: ${_calcularTotal()}'),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        const Text('Total:'),
+                        Text('${_calcularTotal()}'),
+                      ],
+                    ),
                   ),
                 ),
               ],
